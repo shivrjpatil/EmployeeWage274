39e7e52b0f3c5d66cc17da1ade504a8d9d2ed8fb
shivrjpatil <shivrajp972@gmail.com>
2023-04-16 13:45:01 +0530

    Added calculatiion wage using class and method Variables

--- EmployeeWage274/Program.cs
+++ EmployeeWage274/Program.cs
@@ -11,39 +11,8 @@ namespace EmployeeWage274
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Employee Wage Computation Program");
-            const int FULL_TIME = 1;
-            const int PART_TIME = 2;
-            const int WAGE_PER_HOUR = 20;
-            const int MAX_WORKING_DAYS = 20;
-            const int MAX_WORKING_HOURS = 100;
-            int empwage = 0, emphrs = 0, day = 1, totalwage = 0, totalhour = 0;
-
-            Random random = new Random();
-
-            while (day <= MAX_WORKING_DAYS && totalhour <= MAX_WORKING_HOURS)
-            {
-                int EmpCheck = random.Next(0, 3);
-                switch (EmpCheck)
-                {
-
-                    case FULL_TIME:
-                        emphrs = 8;
-                        break;
-
-                    case PART_TIME:
-                        emphrs = 4;
-                        break;
-                    default:
-                        emphrs = 0;
-                        break;
-                }
-                empwage = WAGE_PER_HOUR * emphrs;
-                totalwage += empwage;
-                totalhour += emphrs;
-                day++;
-            }
-            Console.WriteLine("Toatl wage for {0} days:{1} and hour is {2}", (day - 1), totalwage, totalhour);
-            Console.ReadLine();
+           Wage wage = new Wage();
+            wage.CalculateWage();
         }
     }
 }
---
+++ EmployeeWage274/Wage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage274
+{
+    internal class Wage
+    {
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+        public const int WAGE_PER_HOUR = 20;
+        public const int MAX_WORKING_DAYS = 20;
+        public const int MAX_WORKING_HOURS = 100;
+        public void CalculateWage()
+        {
+            int empwage = 0, emphrs = 0, day = 1, totalwage = 0, totalhour = 0;
+
+            Random random = new Random();
+
+            while (day <= MAX_WORKING_DAYS && totalhour <= MAX_WORKING_HOURS)
+            {
+                int EmpCheck = random.Next(0, 3);
+                switch (EmpCheck)
+                {
+
+                    case FULL_TIME:
+                        emphrs = 8;
+                        break;
+
+                    case PART_TIME:
+                        emphrs = 4;
+                        break;
+                    default:
+                        emphrs = 0;
+                        break;
+                }
+                empwage = WAGE_PER_HOUR * emphrs;
+                totalwage += empwage;
+                totalhour += emphrs;
+                day++;
+            }
+            Console.WriteLine("Toatl wage for {0} days:{1} and hour is {2}", (day - 1), totalwage, totalhour);
+            Console.ReadLine();
+        }
+    }
+}
